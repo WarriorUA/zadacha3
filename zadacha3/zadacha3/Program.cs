66138710574eef7ad6905249e0f84939499ed7f3
WarriorUA <dinozawrua@gmail.com>
2016-12-04 21:21:41 +0200

    Add files via upload

---
+++ zadacha3/zadacha3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            List<int> mass = new List<int>();
+            for (int i =0;i<n;++i)
+            {
+                mass.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            mass.Sort();
+            foreach(var t in mass)
+            {
+                Console.WriteLine(t);
+            }
+            Console.ReadKey();
+        }
+    }
+}
